87502d102a85057fce2a4ac63191043c26d6191c
elliotglynn1 <elliotglynn1@gmail.com>
2026-01-17 16:03:09 +0000

    feat: add ebird quuery model

---
+++ api_connectors/ebird/queries.py
@@ -0,0 +1,25 @@
+import requests
+
+class EbirdQueries: 
+  def __init__(self, token: str):
+    self.headers = {
+      'X-eBirdApiToken': token
+    }
+    self.base_url = "https://api.ebird.org/v2/data/obs"
+    self.payload = {}
+
+  def get_recent_observations_in_region(self, region_code: str):
+    response = requests.request("GET", f"{self.base_url}/{region_code}/recent", headers=self.headers, data=self.payload)
+    return response.json()
+
+  def get_recent_notable_observations_in_region(self, region_code: str):
+    response = requests.request("GET", f"{self.base_url}/{region_code}/recent/notable?detail=full", headers=self.headers, data=self.payload)
+    return response.json()
+
+  def get_recent_observations_of_species_in_region(self, region_code: str, species_code: str):
+    response = requests.request("GET", f"{self.base_url}/{region_code}/recent/{species_code}", headers=self.headers, data=self.payload)
+    return response.json()
+
+  def get_recent_nearby_observations(self, lat: float, lng: float):
+    response = requests.request("GET", f"{self.base_url}/geo/recent?lat={lat}&lng={lng}", headers=self.headers, data=self.payload)
+    return response.json()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("I am a keen bird watcher!")
+from api_connectors.ebird.queries import EbirdQueries
+import os 
 
+def main():
+    ebird_queries = EbirdQueries(os.getenv("EBIRD_API_TOKEN"))
+    print(ebird_queries.get_recent_observations_in_region("KZ"))
 
 if __name__ == "__main__":
     main()
